efc254588bc68c1d952356991dff4e58dbe734c3
AbdulrahmanNizar <nizar.zaghdoudi@gmail.com>
2024-08-24 17:42:34 +0300

    Finishing the overview page

--- front-end/src/components/ApiCardsForStores.vue
+++ front-end/src/components/ApiCardsForStores.vue
@@ -1,35 +1,48 @@
 <template>
   <div
-    class="w-100 d-flex flex-column justify-content-center align-items-center border border-secondary rounded p-3"
+    class="p-3 w-100 d-flex flex-column justify-content-center align-items-center rounded border border-secondary text-center"
   >
-    <div
-      class="p-3 w-100 d-flex flex-row justify-content-center align-items-center rounded border border-secondary text-center"
-    >
-      <h5 class="fw-bold">
-        http://192.168.1.241:3000/stores-management/createStore
-      </h5>
-    </div>
-    <div
-      class="p-3 w-100 d-flex flex-row justify-content-center align-items-center rounded border border-secondary mt-2 text-center"
-    >
-      <h5 class="fw-bold">
-        http://192.168.1.241:3000/stores-management/updateStore
-      </h5>
-    </div>
-    <div
-      class="p-3 w-100 d-flex flex-row justify-content-center align-items-center rounded border border-secondary mt-2 text-center"
-    >
-      <h5 class="fw-bold">
-        http://192.168.1.241:3000/stores-management/deleteStore
-      </h5>
-    </div>
-    <div
-      class="p-3 w-100 d-flex flex-row justify-content-center align-items-center rounded border border-secondary mt-2 text-center"
-    >
-      <h5 class="fw-bold">
-        http://192.168.1.241:3000/stores-management/getStore
-      </h5>
-    </div>
+    <h4 class="mb-0">Post</h4>
+    <hr class="w-100" />
+    <h5 class="fw-bold">
+      http://192.168.1.241:3000/stores-management/createStore
+    </h5>
+  </div>
+  <div
+    class="p-3 w-100 d-flex flex-column justify-content-center align-items-center rounded border border-secondary mt-2 text-center"
+  >
+    <h4 class="mb-0">Patch</h4>
+    <hr class="w-100" />
+    <h5 class="fw-bold">
+      http://192.168.1.241:3000/stores-management/updateStore/:storeId
+    </h5>
+  </div>
+  <div
+    class="p-3 w-100 d-flex flex-column justify-content-center align-items-center rounded border border-secondary mt-2 text-center"
+  >
+    <h4 class="mb-0">Delete</h4>
+    <hr class="w-100" />
+    <h5 class="fw-bold">
+      http://192.168.1.241:3000/stores-management/deleteStore/:storeId
+    </h5>
+  </div>
+  <div
+    class="p-3 w-100 d-flex flex-column justify-content-center align-items-center rounded border border-secondary mt-2 text-center"
+  >
+    <h4 class="mb-0">Get</h4>
+    <hr class="w-100" />
+    <h5 class="fw-bold">
+      http://192.168.1.241:3000/stores-management/getStore/:userId
+    </h5>
+  </div>
+  <div
+    class="p-3 w-100 d-flex flex-column justify-content-center align-items-center rounded border border-secondary mt-2 text-center"
+  >
+    <h4 class="mb-0">Get</h4>
+    <hr class="w-100" />
+    <h5 class="fw-bold">
+      http://192.168.1.241:3000/stores-management/getStoreDetails/:storeId
+    </h5>
   </div>
 </template>
 
--- front-end/src/views/Overview.vue
+++ front-end/src/views/Overview.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="h-100">
+  <div class="h-100 text-black" v-if="systemMood == 'light'">
     <nav
       class="navbar navbar-expand-lg d-flex flex-row justify-content-start align-items-start border-bottom"
     >
       <div class="container-fluid w-100">
         <div class="dropdown">
+          <button class="btn btn-light me-1" @click="changeMoodSystem">
+            <i class="bi bi-brightness-high"></i>
+          </button>
           <button
-            class="btn btn-light dropdown-toggle ms-3"
+            class="btn btn-light dropdown-toggle ms-1"
             data-bs-toggle="dropdown"
             aria-expanded="false"
           >
@@ -15,6 +18,18 @@
           </button>
 
           <ul class="dropdown-menu">
+            <div
+              class="d-flex flex-row justify-content-center align-items-center"
+            >
+              <input
+                type="text"
+                placeholder="Search Store"
+                v-model="searchStore"
+                class="form-control"
+                style="width: 90%"
+              />
+            </div>
+            <li><hr class="dropdown-divider" /></li>
             <li
               v-for="store in yourStores"
               class="d-flex flex-row justify-content-center align-items-center"
@@ -70,42 +85,37 @@
               >
             </li>
             <li class="nav-item">
-              <router-link class="nav-link active" :to="{ path: '/billboards' }"
-                >Billboards</router-link
-              >
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link active" :to="{ path: '/categories' }"
+              <router-link class="nav-link" :to="{ path: '/categories' }"
                 >Categories</router-link
               >
             </li>
             <li class="nav-item">
-              <router-link class="nav-link active" :to="{ path: '/sizes' }"
+              <router-link class="nav-link" :to="{ path: '/sizes' }"
                 >Sizes</router-link
               >
             </li>
             <li class="nav-item">
-              <router-link class="nav-link active" :to="{ path: '/colors' }"
+              <router-link class="nav-link" :to="{ path: '/colors' }"
                 >Colors</router-link
               >
             </li>
             <li class="nav-item">
-              <router-link class="nav-link active" :to="{ path: '/products' }"
+              <router-link class="nav-link" :to="{ path: '/products' }"
                 >Products</router-link
               >
             </li>
             <li class="nav-item">
-              <router-link class="nav-link active" :to="{ path: '/orders' }"
+              <router-link class="nav-link" :to="{ path: '/orders' }"
                 >Orders</router-link
               >
             </li>
             <li class="nav-item">
-              <router-link class="nav-link active" :to="{ path: '/settings' }"
+              <router-link class="nav-link" :to="{ path: '/settings' }"
                 >Settings</router-link
               >
             </li>
             <li class="nav-item me-2">
-              <router-link class="nav-link active" :to="{ path: '/account' }"
+              <router-link class="nav-link" :to="{ path: '/account' }"
                 >Account</router-link
               >
             </li>
@@ -123,7 +133,7 @@
       class="d-flex flex-column justify-content-start align-items-start mt-3 p-3 w-100"
     >
       <h3 class="fw-bold ms-5 text-start">Statistics</h3>
-      <p class="text-muted text-start ms-5">Overview of your store</p>
+      <p class="text-start ms-5">Overview of your store</p>
     </div>
 
     <hr class="w-100 text-black" />
@@ -131,30 +141,27 @@
     <div
       class="row mt-2 w-100 d-flex flex-row justify-content-center align-items-center p-2"
     >
-      <div class="ms-3 mt-2 p-4 border border-secondary rounded col-md-3 col-6">
+      <div
+        class="ms-3 mt-2 p-4 border border-secondary rounded col-md-3 col-6"
+        v-for="storeDetail in currentStoreInformation"
+      >
         <h4>Total Revenue</h4>
-        <h2 class="fw-bold">$0</h2>
+        <h2 class="fw-bold">${{ storeDetail.storeTotalRevenue }}</h2>
       </div>
-      <div class="ms-3 mt-2 p-4 border border-secondary rounded col-md-3 col-6">
+      <div
+        class="ms-3 mt-2 p-4 border border-secondary rounded col-md-3 col-6"
+        v-for="storeDetail in currentStoreInformation"
+      >
         <h4>Sales</h4>
-        <h2 class="fw-bold">0</h2>
+        <h2 class="fw-bold">{{ storeDetail.storeSales }}</h2>
       </div>
-      <div class="ms-3 mt-2 p-4 border border-secondary rounded col-md-3 col-6">
+      <div
+        class="ms-3 mt-2 p-4 border border-secondary rounded col-md-3 col-6"
+        v-for="storeDetail in currentStoreInformation"
+      >
         <h4>Products In Stoke</h4>
-        <h2 class="fw-bold">0</h2>
+        <h2 class="fw-bold">{{ storeDetail.storeProducts.length }}</h2>
       </div>
-    </div>
-
-    <hr class="w-100 text-black mt-3" />
-
-    <div class="w-100 text-center">
-      <h3 class="fw-bold">Our Api's</h3>
-    </div>
-
-    <hr class="w-100 text-black" />
-
-    <div class="w-100 p-3">
-      <api-cards-for-stores />
     </div>
 
     <div
@@ -218,19 +225,284 @@
       </div>
     </div>
   </div>
+
+  <div class="h-100 bg-black text-white" v-if="systemMood == 'dark'">
+    <nav
+      class="navbar navbar-expand-lg d-flex flex-row justify-content-start align-items-start border-bottom"
+      data-bs-theme="dark"
+    >
+      <div class="container-fluid w-100">
+        <div class="dropdown">
+          <button class="btn btn-dark me-1" @click="changeMoodSystem">
+            <i class="bi bi-brightness-high"></i>
+          </button>
+          <button
+            class="btn btn-dark dropdown-toggle ms-1"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            <i class="bi bi-shop"></i>
+            {{ currentStoreName }}
+          </button>
+
+          <ul class="dropdown-menu" data-bs-theme="dark">
+            <div
+              class="d-flex flex-row justify-content-center align-items-center"
+            >
+              <input
+                type="text"
+                placeholder="Search Store"
+                v-model="searchStore"
+                class="form-control"
+                style="width: 90%"
+              />
+            </div>
+            <li><hr class="dropdown-divider" /></li>
+            <transition-group
+              :css="false"
+              @before-enter="onBeforeEnter"
+              @enter="onEnter"
+              @leave="onLeave"
+            >
+              <li
+                v-for="store in yourComputedStores"
+                class="d-flex flex-row justify-content-center align-items-center"
+              >
+                <a
+                  href="#"
+                  class="dropdown-item p-2"
+                  @click="manageThisStore(store._id, store.storeName)"
+                >
+                  <i class="bi bi-shop"></i> {{ store.storeName }}
+                  <div class="btn-group ms-2">
+                    <button class="btn btn-light btn-sm">
+                      <i
+                        style="cursor: pointer"
+                        class="bi bi-pencil-square"
+                      ></i>
+                    </button>
+                    <button
+                      class="btn btn-danger btn-sm"
+                      @click="deleteStore(store._id)"
+                    >
+                      <i style="cursor: pointer" class="bi bi-trash"></i>
+                    </button>
+                  </div>
+                </a>
+              </li>
+            </transition-group>
+            <li><hr class="dropdown-divider" /></li>
+            <li>
+              <a
+                href="#"
+                class="dropdown-item"
+                data-bs-toggle="modal"
+                data-bs-target="#createNewStoreModal"
+              >
+                <i class="bi bi-plus"></i> Add New</a
+              >
+            </li>
+          </ul>
+        </div>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse ms-5" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link class="nav-link active" :to="{ path: '/' }"
+                >Overview</router-link
+              >
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" :to="{ path: '/categories' }"
+                >Categories</router-link
+              >
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" :to="{ path: '/sizes' }"
+                >Sizes</router-link
+              >
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" :to="{ path: '/colors' }"
+                >Colors</router-link
+              >
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" :to="{ path: '/products' }"
+                >Products</router-link
+              >
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" :to="{ path: '/orders' }"
+                >Orders</router-link
+              >
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" :to="{ path: '/settings' }"
+                >Settings</router-link
+              >
+            </li>
+            <li class="nav-item me-2">
+              <router-link class="nav-link" :to="{ path: '/account' }"
+                >Account</router-link
+              >
+            </li>
+            <li class="nav-item">
+              <button class="btn btn-outline-danger" @click="logout">
+                Logout
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div
+      class="d-flex flex-column justify-content-start align-items-start mt-3 p-3 w-100"
+    >
+      <h3 class="fw-bold ms-5 text-start">Statistics</h3>
+      <p class="text-start ms-5">Overview of your store</p>
+    </div>
+
+    <hr class="w-100 text-white" />
+
+    <div
+      class="row mt-2 w-100 d-flex flex-row justify-content-center align-items-center p-2"
+    >
+      <div
+        class="ms-3 mt-2 p-4 border border-secondary rounded col-md-3 col-6"
+        v-for="storeDetail in currentStoreInformation"
+      >
+        <h4>Total Revenue</h4>
+        <h2 class="fw-bold">${{ storeDetail.storeTotalRevenue }}</h2>
+      </div>
+      <div
+        class="ms-3 mt-2 p-4 border border-secondary rounded col-md-3 col-6"
+        v-for="storeDetail in currentStoreInformation"
+      >
+        <h4>Sales</h4>
+        <h2 class="fw-bold">{{ storeDetail.storeSales }}</h2>
+      </div>
+      <div
+        class="ms-3 mt-2 p-4 border border-secondary rounded col-md-3 col-6"
+        v-for="storeDetail in currentStoreInformation"
+      >
+        <h4>Products In Stoke</h4>
+        <h2 class="fw-bold">{{ storeDetail.storeProducts.length }}</h2>
+      </div>
+    </div>
+
+    <div
+      class="modal fade"
+      id="createNewStoreModal"
+      data-bs-backdrop="static"
+      data-bs-keyboard="false"
+      tabindex="-1"
+      aria-labelledby="staticBackdropLabel1"
+      aria-hidden="true"
+      data-bs-theme="dark"
+    >
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="staticBackdropLabel1">
+              Create New Store
+            </h1>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">
+            <label for="#newStoreNameInt" class="form-label"
+              >New Store Name</label
+            >
+            <input
+              id="newStoreNameInt"
+              class="form-control mt-1"
+              type="text"
+              v-model="newStoreName"
+              placeholder="Enter A Name For The New Store"
+            />
+            <hr class="w-100 text-black" />
+            <label for="#newStoreAdminsInt" class="form-label"
+              >New Store Admins</label
+            >
+            <input
+              id="newStoreAdminsInt"
+              type="text"
+              class="form-control mt-1"
+              placeholder="Add , Between The Names"
+              v-model="newStoreAdmins"
+            />
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-danger"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+            <button type="button" class="btn btn-light" @click="createNewStore">
+              Create
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import { useRouter } from "vue-router";
+import gsap from "gsap";
 import ApiCardsForStores from "@/components/ApiCardsForStores.vue";
 
 const router = useRouter();
 const userId = ref<string | null>(localStorage.getItem("UserId"));
+const currentStoreId = ref<string | null>(localStorage.getItem("StoreId"));
 const currentStoreName = ref<string | null>(localStorage.getItem("StoreName"));
 const newStoreName = ref<string>("");
 const newStoreAdmins = ref<string>("");
+const searchStore = ref<string>("");
 const yourStores = ref<any>([]);
+const currentStoreInformation = ref<any>([]);
+const systemMood = ref<string | null>(localStorage.getItem("SystemMood"));
+
+const yourComputedStores = computed(() => {
+  return yourStores.value.filter((store: any) =>
+    store.storeName.toLowerCase().includes(searchStore.value)
+  );
+});
+
+if (!systemMood.value) {
+  localStorage.setItem("SystemMood", "light");
+  window.location.reload();
+}
+
+const changeMoodSystem = () => {
+  if (systemMood.value == "light") {
+    localStorage.setItem("SystemMood", "dark");
+    window.location.reload();
+  } else if (systemMood.value == "dark") {
+    localStorage.setItem("SystemMood", "light");
+    window.location.reload();
+  }
+};
 
 const createNewStore = async (): Promise<void> => {
   if (newStoreName.value != "" && newStoreAdmins.value != "") {
@@ -321,6 +593,23 @@ const getYourStores = async (): Promise<void> => {
   }
 };
 
+const getYourStoreInformation = async (): Promise<void> => {
+  if (currentStoreName.value != "") {
+    try {
+      const response = await fetch(
+        `http://192.168.1.241:3000/stores-management/getStoreDetails/${currentStoreId.value}`
+      );
+      const data = await response.json();
+
+      if (data.statusCode >= 200 && data.statusCode < 300) {
+        currentStoreInformation.value = data.data;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+};
+
 const deleteStore = async (storeId: string): Promise<void> => {
   try {
     const requestOptions: any = {
@@ -360,5 +649,29 @@ const manageThisStore = (storeId: string, storeName: string) => {
   }, 10);
 };
 
+function onBeforeEnter(el: any) {
+  el.style.opacity = 0;
+  el.style.height = 0;
+}
+
+function onEnter(el: any, done: any) {
+  gsap.to(el, {
+    opacity: 1,
+    height: "1.6em",
+    delay: el.dataset.index * 0.15,
+    onComplete: done,
+  });
+}
+
+function onLeave(el: any, done: any) {
+  gsap.to(el, {
+    opacity: 0,
+    height: 0,
+    delay: el.dataset.index * 0.15,
+    onComplete: done,
+  });
+}
+
 getYourStores();
+getYourStoreInformation();
 </script>
